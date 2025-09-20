4866b835e0454f3c82c53d85ff90a61885a1ad16
Lincoln Madaraka <madarakalincoln48@gmail.com>
2025-09-20 23:48:48 +0300

    users

--- backend/src/seeds/songs.js
+++ backend/src/seeds/songs.js
@@ -5,8 +5,41 @@ import { config } from "dotenv";
 config();
 
 const songs = [
-	
-
+	{
+    title: "BBB",
+    artist: "Carmen DeLeon",
+    imageUrl: "/cover-images/1.jpg", // pick any cover image you want
+    audioUrl: "/songs/Carmen DeLeon - BBB .mp3",
+    duration: 0, // update later with real duration if needed
+  },
+  {
+    title: "Mala memoria",
+    artist: "Carmen DeLeon",
+    imageUrl: "/cover-images/2.jpg",
+    audioUrl: "/songs/Carmen DeLeon - Mala memoria .mp3",
+    duration: 0,
+  },
+  {
+    title: "On Yah",
+    artist: "Nikita Kering",
+    imageUrl: "/cover-images/3.jpg",
+    audioUrl: "/songs/Nikita Kering - On Yah .mp3",
+    duration: 0,
+  },
+  {
+    title: "Vamp",
+    artist: "Travis Scott & Playboi Carti",
+    imageUrl: "/cover-images/4.jpg",
+    audioUrl: "/songs/Travis Scott _ Playboi Carti - Vamp.mp3",
+    duration: 0,
+  },
+  {
+    title: "Shu-Peru",
+    artist: "Kizz Daniel",
+    imageUrl: "/cover-images/5.jpg",
+    audioUrl: "/songs/Kizz Daniel - Shu-Peru .mp3",
+    duration: 0,
+  },
 ];
 
 const seedSongs = async () => {
